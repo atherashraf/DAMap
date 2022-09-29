3249e5e9e12900465e7d2602440cc42e7b52014e
atherashraf <athear.ashraf@mic3.com>
2022-09-29 03:07:15 +0300

    TS strict checking

--- src/ol-map/layers/MVTLayer.ts
+++ src/ol-map/layers/MVTLayer.ts
@@ -91,7 +91,8 @@ class MVTLayer extends AbstractVectorLayer {
         let style: IGeomStyle;
         let rules: IRule[]
         let properties: any
-        switch (this.style?.type) {
+        const type= this.style?.type || ""
+        switch (type) {
             case "single":
                 style = this.style["style"]["default"];
                 break;
